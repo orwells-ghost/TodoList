94125e505b9088143f04def39b1f4c1cd2dfda7a
Ryan Kennedy <33361057+orwells-ghost@users.noreply.github.com>
2018-05-22 21:01:29 -0700

    Added logic

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,4 +1,21 @@
 //Strike through todos
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed")
+});
+
+//Click on X span to delete
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(600,function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+//Get input from user and add to todo list
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
 });
